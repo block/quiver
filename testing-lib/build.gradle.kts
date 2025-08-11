39d9af6ad4ea72480af03403bfc33bad46512fc9
Hugo <hugom@squareup.com>
2025-08-11 17:24:55 +1000

    Removes mavenPublishing block from testing-lib (#132)

--- testing-lib/build.gradle.kts
+++ testing-lib/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   id("java-library")
   id("org.jetbrains.kotlin.jvm")
-  id("com.vanniktech.maven.publish")
 }
 
 repositories {
@@ -19,10 +18,6 @@ java {
     languageVersion.set(JavaLanguageVersion.of(11))
   }
   withSourcesJar()
-}
-
-mavenPublishing {
-  configure(com.vanniktech.maven.publish.KotlinJvm())
 }
 
 dependencies {
